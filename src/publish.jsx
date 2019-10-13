b1c32aa8e3627651a9947f8a8428eb21f37f1fde
Victor Jimenez <vic.jicama@gmail.com>
2019-10-13 21:27:56 +0200

    Change default endpoint to build.repoflow.local

--- src/publish.jsx
+++ src/publish.jsx
@@ -1,7 +1,7 @@
 import { Publish } from "@nebulario/core-plugin-request";
 
 export const publish = async (params, cxt) => {
-  let serverUrl = "http://localhost:8000/build";
+  let serverUrl = "http://build.repoflow.local:8000/build";
 
   if (params.config.build) {
     serverUrl = params.config.build.url;
